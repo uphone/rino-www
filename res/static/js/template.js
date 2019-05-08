d0fd5b4d4c631a2b8c1d3005a1e49bfd1fa10705
uphone <595228808@qq.com>
2019-05-08 10:54:35 +0800

    init

---
+++ res/static/js/template.js
@@ -0,0 +1,31 @@
+const __FOOTER = '<div class="footer">' +
+'<div class="layui-container">' +
+'<p class="footer-web">' +
+'<a href="javascript:;">友情链接</a>' +
+'<a href="http://www.chinagreentown.com/" target="_blank">绿城集团</a>' +
+'<a href="https://www.hwabaotrust.com/" target="_blank">华宝信托</a>' +
+'<a href="http://bg.baosteel.com/" target="_blank">宝钢集团</a>' +
+'<a href="http://www.chinakingking.com/" target="_blank">青岛金王</a>' +
+'</p>' +
+'<div class="layui-row footer-contact">' +
+'<div class="layui-col-sm2 layui-col-lg1"><img src="res/static/img/ewm.png" style="width:86px;height:86px;"></div>' +
+'<div class="layui-col-sm10 layui-col-lg11">' +
+'<div class="layui-row">' +
+'<div class="layui-col-sm6 layui-col-md8 layui-col-lg8">' +
+'<p class="contact-top">' +
+'<i class="layui-icon layui-icon-cellphone"></i>&nbsp;130-20191238&nbsp;&nbsp;&nbsp;(9:00-18:00)' +
+'</p>' +
+'<p class="contact-top">' +
+'<i class="layui-icon layui-icon-cellphone"></i>&nbsp;131-62100298&nbsp;&nbsp;&nbsp;(9:00-18:00)' +
+'</p>' +
+'<p class="contact-bottom"><i class="layui-icon layui-icon-home"></i>&nbsp;sales@rino123.com</span></p>' +
+'</div>' +
+'<div class="layui-col-sm6 layui-col-md4 layui-col-lg4">' +
+'<p class="contact-top"><span class="right">上海市奉贤区程普路377号</span></p>' +
+'<p class="contact-bottom"><span class="right">Copyright&nbsp;©&nbsp;2011-2019 沪ICP备15011502号-1</span></p>' +
+'</div>' +
+'</div>' +
+'</div>' +
+'</div>' +
+'</div>' +
+'</div>';
